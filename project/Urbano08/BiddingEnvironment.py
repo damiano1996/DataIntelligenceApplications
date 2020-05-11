a37164ec4c5361d9d5c480fe009bc495466ccc15
Enrico <enrico.voltan@mail.polimi.it>
2020-05-11 18:29:57 +0200

    Learner e plot di 1 subcampaign

--- project/Urbano08/BiddingEnvironment.py
+++ project/Urbano08/BiddingEnvironment.py
@@ -9,17 +9,18 @@ class BiddingEnvironment():
         self.max = max
 
     def bid_sub1(self,x):
-        return self.max * (1.0 - np.exp(-(8 * x)))
+        #return self.max * (1.0 - np.exp(-(4 * x)))
+        return self.max * (1.0 - np.exp(-4 * x))
 
     def bid_sub2(self,x):
-        return self.max * (1.0 - np.exp(-(5 * x)))
+        return self.max * (1.0 - np.exp(-5 * x))
 
     def bid_sub3(self,x):
-        return self.max * (1.0 - np.exp(-(3 * x)))
+        return self.max * (1.0 - np.exp(-3 * x))
 
 
     def round(self, pulled_arm1,pulled_arm2, pulled_arm3):
 
-        return (np.random.normal(self.bid_sub1(pulled_arm1), self.sigma) ,
-               np.random.normal(self.bid_sub2(pulled_arm2), self.sigma) ,
-               np.random.normal(self.bid_sub3(pulled_arm3), self.sigma) )
+        return (np.random.normal(self.bid_sub1(self.bids[pulled_arm1]), self.sigma) ,
+               np.random.normal(self.bid_sub2(self.bids[pulled_arm2]), self.sigma) ,
+               np.random.normal(self.bid_sub3(self.bids[pulled_arm3]), self.sigma) )
---
+++ project/Urbano08/Learner.py
@@ -0,0 +1,54 @@
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+
+
+
+class Learner():
+
+    def __init__(self, n_arms, arms):
+        self.n_arms = n_arms
+
+        self.t = 0
+        self.rewards_per_arm = x = [[] for _ in range(n_arms)]
+        self.collected_rewards = np.array([])
+
+        self.arms = arms
+        self.means = np.zeros(n_arms)
+        self.sigmas = np.ones(n_arms) * 10
+        self.pulled_arms = []
+
+        alpha = 10.0
+        kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3))
+        self.gp = GaussianProcessRegressor(kernel=kernel,
+                                           alpha=alpha ** 2,
+                                           normalize_y=True,
+                                           n_restarts_optimizer=9)
+
+        self.X = []
+        self.Y = []
+
+    def update_observations(self, arm_idx, reward):
+        self.rewards_per_arm[arm_idx].append(reward)
+        self.pulled_arms.append(self.arms[arm_idx])
+        self.collected_rewards = np.append(self.collected_rewards, reward)
+
+    def update_model(self):
+        x = np.atleast_2d(self.pulled_arms).T
+        y = self.collected_rewards
+
+        self.X = x
+        self.Y = y.ravel()
+
+        self.gp.fit(x, y)
+        self.means, self.sigmas = self.gp.predict(np.atleast_2d(self.arms).T, return_std=True)
+        self.sigmas = np.maximum(self.sigmas, 1e-2)
+
+    def update(self, pulled_arm, reward):
+        self.t += 1
+        self.update_observations(pulled_arm, reward)
+        self.update_model()
+
+    def pull_arm(self):
+        sampled_values = np.random.normal(self.means, self.sigmas)
+        return np.argmax(sampled_values)
