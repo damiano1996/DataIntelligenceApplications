40ced30708418a5328e708428c974802322a318d
Andre4102 <diecidue.andrea@gmail.com>
2020-05-13 20:22:51 +0200

    added context generator

--- project/part_5/Context.py
+++ project/part_5/Context.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-from project.dia_pckg.Environment import Environment
+from project.part_4.Env_4 import Env_4
 
 
-class Context(Environment):
+class Context(Env_4):
 
-    def __init__(self, context_name, classes, initial_date, n_days, users_per_day, n_arms):
-        '''
+    def __init__(self, context_name, classes, initial_date, n_days, users_per_day, n_arms, feature):
+        """
         constructor for the class, when called it creates the demand curve for the context
         based on the classes
         :param context_name:
@@ -15,10 +15,11 @@ class Context(Environment):
         :param n_days:
         :param users_per_day: number of rounds for each day
         :param n_arms: number of arms of TS algorithm
-        '''
+        """
 
         super().__init__(initial_date, n_days)
 
+        self.feature = feature
         self.name = context_name
         self.classes = classes
         self.n_classes = classes.len()
@@ -36,57 +37,7 @@ class Context(Environment):
             self.classes = {'class_1': classes[0], 'class_2': classes[1]}
         else:
             self.classes = {'class_1': classes[0]}
-
         self.context_demand_curve = self.get_aggregate_curve()
-
-    def user_step(self, pulled_arm, user):
-        """
-            This method performs a round considering the number of steps per day
-            Only after n rounds it perform a step in the implemented class
-        :param pulled_arm: arm to pull
-        :param user: User object
-        :return: (reward, current date, done) done is a boolean -> True if the "game" is finished
-        """
-        reward, opt_revenue = self.round(pulled_arm, user)
-
-        current_date = self.get_current_date()
-        done = False
-
-        self.count_rounds_today += 1
-        if self.count_rounds_today == self.round_per_day:
-            self.count_rounds_today = 0
-            current_date, done = self.step()
-
-        return reward, current_date, done, opt_revenue
-
-    def round(self, pulled_arm, user):
-        """
-            This method performs a round taking the probability from the user's class
-        :param pulled_arm: arm to pull
-        :param user: User object
-        :return: reward
-        """
-        # class of the user
-        conv_rate = None
-        for class_ in self.classes.values():
-            if user.class_name == class_.name:
-                conv_rate = class_.conv_rates['phase_0']  # taking the first because no abrupt phases
-
-        # taking the probability from the conversion curve, associated to the pulled_arm
-        probability = conv_rate['probabilities'][self.arm_prices['indices'][pulled_arm]]
-        optimals = self.get_optimal_price(conv_rate)
-        optimal_revenue = optimals['price'] * optimals['probability']
-
-        reward = np.random.binomial(1, probability)
-        return reward, optimal_revenue
-
-    def reset(self):
-        """
-            to reset the environment
-        :return: None
-        """
-        self.count_rounds_today = 0
-        return super(Context, self).reset()
 
     def get_aggregate_curve(self):
         """
@@ -128,25 +79,3 @@ class Context(Environment):
 
         return {'context': self.get_optimal_price(self.context_demand_curve),
                 'classes': optimal_classes}
-
-    def get_optimal_price(self, conv_rate):
-        """
-            This method computes the max area
-        :param conv_rate: (price, probability)
-        :return:
-        """
-        areas = conv_rate['prices'] * conv_rate['probabilities']
-        idx = np.argmax(areas)
-        return {'price': conv_rate['prices'][idx],
-                'probability': conv_rate['probabilities'][idx]}
-
-    def get_candidate_prices(self):
-        """
-            This method return the candidate prices, one price for each arm.
-            The "indices" array contains the positions of the specified prices in the aggregate curve
-        :return:
-        """
-        arm_distance = int(self.context_demand_curve['prices'].shape[0] / self.n_arms)
-        idx = [int(arm_distance * arm) for arm in range(self.n_arms)]
-        prices = self.context_demand_curve['prices'][idx]
-        return {'indices': idx, 'prices': prices}
--- project/part_5/ContextGenerator.py
+++ project/part_5/ContextGenerator.py
@@ -1,14 +1,149 @@
-class ContextGenerator():
+from project.part_5.Context import Context
+import math
+import numpy as np
 
-    def __init__(self):
+
+class ContextGenerator:
+
+    def __init__(self, class_1, class_2, class_3, feature_1, feature_2):
+        """
+
+        :param class_1: class object
+        :param class_2: class object
+        :param class_3: class object
+        """
+        self.classes = {class_1.features: class_1, class_2.features: class_2, class_3.features: class_3, }
+        self.features = {feature_1, feature_2}
         pass
 
-    def get_weekly_contexts(self, features_space, users):
+    def get_weekly_contexts(self, users, contexts, sales, date, n_arms, users_per_day):
         """
-        :param features_space: features space
-        :param users: list of User object: they are users collected from the beginning of the campaign
+        :param users: dictionary of class->number of users, they are users collected from the beginning of the campaign
+        :param contexts: the list of contexts used in the previous iteration
+        :param sales: dictionary class->sold items during the week
+        :param date: starting date for a new context
+        :param n_arms: arms of the bandit for this context
+        :param users_per_day: estimated users in a day for each class
         :return: dictionary of the following shape:
                 {'context_1': features, 'context_2': features, ...}
                     where features is a list containing the features of the context: e.g. ['<30', 'worker]
         """
-        pass
+
+        if contexts.len() == 1:
+            cont = {}
+            not_cont = {}
+            low_bound = {}
+            for feature in self.features:
+                cont[feature], not_cont[feature] = self.get_classes(feature, contexts['context_1'])
+                low_bound[feature] = self.get_low_bound(cont, not_cont, users, sales)
+            best_feature = np.argmax(low_bound)
+            if low_bound[best_feature] > self.get_low_bound(contexts['context_1'].classes, [], users, sales):
+                #split
+                new_contexts = {
+                    'context_1': Context(classes=cont[best_feature], feature=best_feature, initial_date=date,
+                                         n_arms=n_arms, users_per_day=users_per_day, n_days=7, context_name='giuseppe'),
+                    'context_2': Context(classes=not_cont[best_feature], feature=best_feature, initial_date=date,
+                                         n_arms=n_arms, users_per_day=users_per_day, n_days=7, context_name='giuseppe')}
+            else:
+                #no split
+                new_contexts = {'context_1': Context(classes=contexts['context_1'].classes, feature=contexts['context_1'].feature,
+                                                     initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                                     context_name=contexts['context_1'].context_name)}
+
+        elif contexts.len() == 2:
+            if contexts['context_1'].feature == 'age':
+                cont, not_cont = self.get_classes('profession', contexts['context_1'])
+                feature = 'profession'
+                low_bound = self.get_low_bound(cont, not_cont, users, sales)
+            else:
+                cont, not_cont = self.get_classes('age', contexts['context_1'])
+                feature = 'age'
+                low_bound = self.get_low_bound(cont, not_cont, users, sales)
+            # add possibility to recombine contexts
+            if low_bound > self.get_low_bound(contexts[0].classes, [], users, sales):
+                #split
+                new_contexts = {
+                    'context_1': Context(classes=cont, feature=feature, initial_date=date,
+                                         n_arms=n_arms, users_per_day=users_per_day, n_days=7, context_name='giuseppe'),
+                    'context_2': Context(classes=not_cont, feature=feature, initial_date=date,
+                                         n_arms=n_arms, users_per_day=users_per_day, n_days=7, context_name='giuseppe'),
+                    'context_3': Context(classes=contexts['context_2'].classes, feature=contexts['context_2'].feature,
+                                         initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                         context_name=contexts['context_2'].context_name)
+                }
+            else:
+                #no split
+                new_contexts = {
+                    'context_1': Context(classes=contexts['context_1'].classes, feature=contexts['context_1'].feature,
+                                         initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                         context_name=contexts['context_1'].context_name),
+                    'context_2': Context(classes=contexts['context_2'].classes, feature=contexts['context_2'].feature,
+                                         initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                         context_name=contexts['context_2'].context_name)
+                }
+        else:
+            # add possibility to recombine contexts
+            new_contexts = {
+                'context_1': Context(classes=contexts['context_1'].classes, feature=contexts['context_1'].feature,
+                                     initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                     context_name=contexts['context_1'].context_name),
+                'context_2': Context(classes=contexts['context_2'].classes, feature=contexts['context_2'].feature,
+                                     initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                     context_name=contexts['context_2'].context_name),
+                'context_3': Context(classes=contexts['context_3'].classes, feature=contexts['context_3'].feature,
+                                     initial_date=date, n_arms=n_arms, users_per_day=users_per_day, n_days=7,
+                                     context_name=contexts['context_3'].context_name)
+            }
+
+        return new_contexts
+
+    def get_classes(self, feature, context):
+        """
+        this function splits according to the considered feature
+        :param feature: the feature we want to evaluate
+        :param context: the context we want to split
+        :return:
+        """
+        class_cont = {}
+        class_not_cont = {}
+        if context.n_classes == 3:
+            if feature == 'age':
+                class_cont = {'class_1': self.classes[{'<30', 'student'}], 'class_2': self.classes[{'<30', 'worker'}]}
+                class_not_cont = self.classes[{'>30', 'worker'}]
+            else:
+                class_cont = {'class_1': self.classes[{'>30', 'worker'}], 'class_2': self.classes[{'<30', 'worker'}]}
+                class_not_cont = self.classes[{'<30', 'student'}]
+
+        elif context.n_classes == 2:
+            if feature == 'age':
+                class_cont = self.classes[{'>30', 'worker'}]
+                class_not_cont = self.classes[{'<30', 'worker'}]
+            else:
+                class_cont = self.classes[{'<30', 'worker'}]
+                class_not_cont = self.classes[{'<30', 'student'}]
+
+        return class_cont, class_not_cont
+
+    def get_low_bound(self, cont, not_cont, users, sales):
+        """
+        this function evaluates a feature based on the classes that it splits
+        :param sales:
+        :param users: number of users in the last week for each class
+        :param cont: the class we want to put in the context
+        :param not_cont: the class we don't want to put in the context
+        :return: the lower bound of the context to be generated
+        """
+        delta_1 = 5
+        z_1 = 0
+        x_1 = 0
+        delta_2 = 5
+        z_2 = 0
+        x_2 = 0
+        for classe in cont:
+            z_1 += users[classe]
+            x_1 += sales[classe]
+        for classe in not_cont:
+            z_2 += users[classe]
+            x_2 += sales[classe]
+        tot = z_1 + z_2
+        return z_1/tot *(x_1 - math.sqrt(-math.log(delta_1) / 2 * z_1)) + z_2/tot*(x_2 - math.sqrt(-math.log(delta_2) / 2 * z_2))
