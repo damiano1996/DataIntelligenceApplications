ece6fd2ee59e54ca28eb18e84b5b48e5ac418e4e
Enrico <enrico.voltan@mail.polimi.it>
2020-05-13 22:11:22 +0200

    Algoritmo tabella finito
    Ora bisogna fare il grafico del regret

--- project/Urbano08/BiddingEnvironment.py
+++ project/Urbano08/BiddingEnvironment.py
@@ -20,8 +20,11 @@ class BiddingEnvironment():
         return self.max * (1.0 - np.exp(-0.9 * x))
 
     def round(self, pulled_arm1, pulled_arm2, pulled_arm3):
-
-        return np.array([np.maximum(0,np.random.normal(self.bid_sub1(self.bids[pulled_arm1]), self.sigma)),
-                np.maximum(0,np.random.normal(self.bid_sub2(self.bids[pulled_arm2]), self.sigma)),
-                np.maximum(0,np.random.normal(self.bid_sub3(self.bids[pulled_arm3]), self.sigma))])
+        reward1 = 0 if pulled_arm1 == 0 else np.maximum(0,np.random.normal(self.bid_sub1(self.bids[pulled_arm1]), self.sigma))
+        reward2 = 0 if pulled_arm2 == 0 else np.maximum(0,np.random.normal(self.bid_sub2(self.bids[pulled_arm2]), self.sigma))
+        reward3 = 0 if pulled_arm3 == 0 else np.maximum(0,np.random.normal(self.bid_sub3(self.bids[pulled_arm3]), self.sigma))
+        return np.array([reward1,reward2,reward3])
+        #return np.array([np.maximum(0,np.random.normal(self.bid_sub1(self.bids[pulled_arm1]), self.sigma)),
+        #        np.maximum(0,np.random.normal(self.bid_sub2(self.bids[pulled_arm2]), self.sigma)),
+        #        np.maximum(0,np.random.normal(self.bid_sub3(self.bids[pulled_arm3]), self.sigma))])
 
