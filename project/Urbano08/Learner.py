7c8665906333adda640719649d48990b63076cf6
antonio-urbano <37112776+antonio-urbano@users.noreply.github.com>
2020-05-20 16:16:53 +0200

    restored last version of learner
    The problem with the gp process must be solved when the last version (0.23) of sklearn is used

--- project/Urbano08/Learner.py
+++ project/Urbano08/Learner.py
@@ -20,12 +20,12 @@ class Learner():
 
         alpha = 10.0
         kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3))
-        #self.gp = GaussianProcessRegressor(kernel=kernel,
-        #                                   alpha=alpha ** 2,
-        #                                   normalize_y=True,
-        #                                   n_restarts_optimizer=9)
-        self.gp = GaussianProcessRegressor(normalize_y=True,
+        self.gp = GaussianProcessRegressor(kernel=kernel,
+                                           alpha=alpha ** 2,
+                                           normalize_y=True,
                                            n_restarts_optimizer=9)
+        #self.gp = GaussianProcessRegressor(normalize_y=True,
+         #                                  n_restarts_optimizer=9)
         self.X = []
         self.Y = []
 
