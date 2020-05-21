6dfe3e6300c5027f8f24863fa74d48217d5f96ee
Enrico <enrico.voltan@mail.polimi.it>
2020-05-21 10:18:40 +0200

    Table Algorithm class

--- project/Urbano08/TableAlgorithm.py
+++ project/Urbano08/TableAlgorithm.py
@@ -7,7 +7,7 @@ class TableAlgorithm(np):
         cols = table_all_Subs.shape[1]
         algorithm_table = np.ndarray(shape=(0, cols), dtype=float)
 
-        for x in raws:
+        for r in raws:
             algorithm_table = np.append(algorithm_table, np.atleast_2d(np.zeros(cols)), 0)
 
         algorithm_table[0, :] = table_all_Subs[0, :]
@@ -21,16 +21,11 @@ class TableAlgorithm(np):
                 possibilities = np.array([])
                 poss_allocation = []
                 for p in range(0, j + 1):
-                    # if bids[p] + bids[j-p] <= bids[j] :
                     possibilities = np.append(possibilities, table_all_Subs[i, p] + algorithm_table[i - 1, j - p])
                     poss_allocation.append(allocations_table[i - 1][j - p] + [p])
-                    # print(str(i)+" "+ str(p) + " "+ str(j-p))
 
                 max_index = np.argmax(possibilities)
-                # print(poss_allocation)
-                # print(possibilities)
 
-                # print(max_index)
                 algorithm_table[i, j] = possibilities[max_index]
                 allocations_table[i][j] = poss_allocation[max_index]
         return allocations_table[raws-1][cols-1],max(algorithm_table[cols-1])
