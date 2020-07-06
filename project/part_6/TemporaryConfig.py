7f1882197b5be2c342da519ef6e63c4a00662ed5
AndreaBiondaPolimi <andrea.bionda@mail.polimi.it>
2020-07-06 14:32:31 +0200

    minor changes on part 6

--- project/part_6/TemporaryConfig.py
+++ project/part_6/TemporaryConfig.py
@@ -1,8 +1,8 @@
 #da togliere quando ci sarà unione dei config
 
-max_bid = 1 
-max_clicks = 1000
-classes_config = {
+max_bid = 1 #potrebbe essere seller_max_budget in dia_pkg.Config.py 
+max_clicks = 1000 #potrebbe essere max_n_clicks in dia_pkg.Config.py 
+classes_config = { #unire bidding environment con multiclasshandler
     'elegant': 0,  
     'casual':  1,  
     'sports':  2  
