3877c68943d6c58a877b3b4117a37bd182076e25
Enrico <enrico.voltan@mail.polimi.it>
2020-07-16 10:08:09 +0200

    updated ex3 with new static learner, and updated DynamicLearner.py with new way of updating the sliding window

--- project/part_3/DynamicLearner.py
+++ project/part_3/DynamicLearner.py
@@ -9,18 +9,18 @@ class DynamicLearner(GPTS_Learner):
 
     def update_observations(self, arm_idx, reward):
         if len(self.collected_rewards) == self.len_window:
-            try:
-                index_to_pop = np.where(self.pulled_arms == self.arms[arm_idx])[0][0]
-                self.rewards_per_arm[arm_idx].pop(0)
-                self.pulled_arms.pop(index_to_pop)
-                self.collected_rewards = np.delete(self.collected_rewards, index_to_pop)
-
-
-            except:
-                index_arm = np.where(self.arms == self.pulled_arms[0])[0][0]
-                self.rewards_per_arm[index_arm].pop(0)
-                self.pulled_arms.pop(0)
-                self.collected_rewards = np.delete(self.collected_rewards, 0)
+            # try:
+            #     index_to_pop = np.where(self.pulled_arms == self.arms[arm_idx])[0][0]
+            #     self.rewards_per_arm[arm_idx].pop(0)
+            #     self.pulled_arms.pop(index_to_pop)
+            #     self.collected_rewards = np.delete(self.collected_rewards, index_to_pop)
+            #
+            #
+            # except:
+            index_arm = np.where(self.arms == self.pulled_arms[0])[0][0]
+            self.rewards_per_arm[index_arm].pop(0)
+            self.pulled_arms.pop(0)
+            self.collected_rewards = np.delete(self.collected_rewards, 0)
 
         # if len(self.collected_rewards) == self.len_window:
         #    index_arm = np.where(self.arms == self.pulled_arms[0])[0][0]
